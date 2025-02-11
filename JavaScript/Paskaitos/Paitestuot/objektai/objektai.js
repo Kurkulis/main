7bbe94d3549df398614712b0c845914efde9efd5
Kurkulis <ignas.kurkulis@gmail.com>
2025-02-11 05:56:39 +0200

    mokiausi

---
+++ JavaScript/Paskaitos/Paitestuot/objektai/objektai.js
@@ -0,0 +1,76 @@
+// function test(num) {
+// return num % 2 === 0
+// }
+// console.log(test(4))
+
+// function bigger(num1,num2) {
+//     return num1 > num2 ? num1 : num2;
+// }
+// console.log(bigger(4,3))
+
+// function padvigubinam(masyvas) {
+//     return masyvas.map(item => item * 2);
+// }
+// console.log(padvigubinam([1, 2, 3]));
+
+
+// function pirma(callback) {
+//     console.log("Sveikas!");
+//     callback();
+// }
+// function antra() {
+//     console.log("Sveeeeeikas!");
+// }
+// pirma(antra)
+
+
+// function labas(name) {
+//     return `labas, ${name}`;
+// }
+// console.log(labas('Ignai'));
+
+// const square = num => num * num;
+// console.log(square(4));
+
+
+// function multiplyByTwo(item) {
+// return item*2;
+// }
+// console.log(multiplyByTwo(4))
+
+// function filterEvenNumbers(arr) {
+//     return arr.filter(item => item % 2 === 0)
+// }
+// console.log(filterEvenNumbers([1,2,3,4,5,6,7,8]))
+
+// function doTask(callback) {
+//     console.log('zdrv');
+//     callback();
+// }
+// function afterTask() {
+//     console.log('labas');
+// }
+// doTask(afterTask);
+
+// function getStatus(num) {
+//     if (num > 10) {
+//         return 'daugiau nei 10';
+//     } else if (num < 10) {
+//         return 'maziau nei 10';
+//     } else {
+//     return 'lygus 10'}
+// }
+// console.log(getStatus(1));
+
+// function skaiciuok(num1,num2,zenk) {
+// if (zenk === "+") {
+//     return num1 + num2;
+// } else if (zenk === "-") {
+//     return num1 - num2;
+// } else if (zenk === "*") {
+//     return num1 * num2;
+// } else {
+//     return 'lievas zenklas'
+// }
+// }
+// console.log(skaiciuok(1,2,";"))

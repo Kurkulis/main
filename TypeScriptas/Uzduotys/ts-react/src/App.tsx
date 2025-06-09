f90a3c46bb12f79015ad06b12a17cf8267dcacb2
Kurkulis <ignas.kurkulis@gmail.com>
2025-06-09 20:18:28 +0300

    ts pradzia

---
+++ TypeScriptas/Uzduotys/ts-react/src/App.tsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+
+  // pirmoji uzduotis
+
+  const name: string = 'john';
+  const amzius: number = 95;
+  console.log(name, amzius);
+
+  // antroji uzduotis
+
+  let kazkas = 'monkey';
+  console.log(kazkas);
+
+  // trecioji uzduotis
+  let tekstas = "123";
+  console.log(typeof tekstas)
+  let skaicioks = parseInt(tekstas);
+  console.log(typeof skaicioks)
+
+  // ketvirtoji uzduotis
+
+  let masyvas: any[] = [123, 321, 526, 452]
+  console.log(masyvas)
+  masyvas.push(123)
+  console.log(masyvas)
+  masyvas.pop()
+  console.log(masyvas)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
